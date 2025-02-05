1c987ca4a8e76a71b8132b14ed365b426707148a
arif6474 <arifulislam64743@gmail.com>
2025-02-05 19:45:04 +0600

    anthill-main

--- apps/anthill/src/App.jsx
+++ apps/anthill/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import './App.css'
-function Home() {
+// apps/main/src/App.tsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Navbar from "./components/Navbar";
+import Contact from "./pages/contact/Contact";
+import "./App.css";
+function App() {
   return (
-    <div>Home page - anthill</div>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default Home
+export default App;
---
+++ apps/anthill/src/pages/contact/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>Contact page - anthill</div>
+  )
+}
+
+export default Contact
